3778585df98a522e0fa95063d60bc63417acd679
Julien <julien22130@gmail.com>
2021-07-26 15:17:55 +0200

    Init

---
+++ src/main/java/metier/Jeu.java
@@ -0,0 +1,49 @@
+package metier;
+
+import java.util.List;
+
+public class Jeu {
+
+	private String titre;
+	private Console console;
+	
+	
+	
+	public Jeu(String titre, Console console) {
+		this.titre = titre;
+		this.console = console;
+	}
+
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+
+
+	public Console getConsole() {
+		return console;
+	}
+
+
+
+	public void setConsole(Console console) {
+		this.console = console;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Jeu [titre=" + titre + ", console=" + console + "]";
+	}
+	
+	
+}
