09063fb8f2b59deff8808f484c6892dbe5717905
Julien <julien22130@gmail.com>
2021-07-26 15:38:01 +0200

    Add 5 Jeu 2 Console

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,24 @@
 package test;
 
+import metier.Console;
+import metier.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Console SuperNintendo = new Console("SuperNintendo");
+		Console XBoite = new Console("XBoite");
+		
+		
+		
+		Jeu MarioTurfu = new Jeu("MarioTurfu", SuperNintendo);
+		Jeu Pong3D = new Jeu("Pong3D", XBoite);
+		Jeu TurboRace = new Jeu("TurboRace", SuperNintendo);
+		Jeu Bagarre = new Jeu("Bagarre", SuperNintendo);
+		Jeu Karting = new Jeu("Karting", XBoite);
+		
 
 	}
 
