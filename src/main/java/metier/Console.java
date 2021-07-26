817e018e3b3f34b2786b26fbd8018e07aa9d4036
Marie <mkerbiriou@gmail.com>
2021-07-26 16:17:18 +0200

    attributs console

--- src/main/java/metier/Console.java
+++ src/main/java/metier/Console.java
@@ -1,11 +1,19 @@
 package metier;
 
+import java.time.LocalDate;
+
 public abstract class Console {
 
 	private String nom;
+	private int prix;
+	private LocalDate dateSortie;
 
-	public Console(String nom) {
+	
+
+	public Console(String nom, int prix, LocalDate dateSortie) {
 		this.nom = nom;
+		this.prix = prix;
+		this.dateSortie = dateSortie;
 	}
 
 	public String getNom() {
